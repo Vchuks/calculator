6f9f1f6c7ca98ac227cb61c81f8f74d9f39d041e
Vchuks <64593598+Vchuks@users.noreply.github.com>
2022-04-30 10:36:54 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import logo from './images/dictionary.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>hello people</h1>
+      <img src={logo} />
     </div>
   );
 }
